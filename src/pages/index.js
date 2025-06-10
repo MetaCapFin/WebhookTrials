de4a2b31196b9ffe14ae99e75c319e575c317e19
MetaCapFin <advisor@metacap.finance>
2025-06-10 14:14:24 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import FreeCourseGuide from '../components/FreeCourseGuide'
-import ChatbotWidget from '../components/ChatbotWidget'
+import FreeCourseGuide from '../components/FreeCourseGuide';
+import ChatbotWidget from '../components/ChatbotWidget';
 
 export default function Home() {
   return (
-    <main className="relative">
-      <FreeCourseGuide />
+    <>
+      <main className="min-h-screen">
+        <FreeCourseGuide />
+      </main>
+
+      {/* Haro Chat Floating Button (always fixed above everything) */}
       <ChatbotWidget />
-    </main>
-  )
+    </>
+  );
 }
 
 
