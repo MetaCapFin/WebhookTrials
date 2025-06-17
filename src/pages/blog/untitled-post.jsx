4f06228244ce726b70948838e663cb7a267ee857
MetaCapFin <advisor@metacap.finance>
2025-06-17 12:02:18 -0500

    Add new blog post from Google Docs

---
+++ src/pages/blog/untitled-post.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+export default function untitledpost() {
+  return (
+    <div className="blog-post">
+      <h1>Untitled Post</h1>
+      <p><em>2025-06-17</em></p>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
